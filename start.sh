e7db23d7d32e184ae3976f52a919c4d024312aa4
Elena Agafoshina <eleonora@MacBook-Pro-von-Elena.local>
2025-04-06 17:33:58 +0200

    переместила и обновила start.sh

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ then
 fi
 
 # Запуск основного Telegram-бота (webhook_bot.py) с логированием ошибок
-python3 webhook_bot.py 2> error.log
+python3 webhook_bot.py 2>&1 | tee error.log
 
 # Проверка, завершился ли бот с ошибкой
 if [ $? -ne 0 ]; then
